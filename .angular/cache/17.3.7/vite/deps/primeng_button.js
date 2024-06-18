a14e3f2e8b07f72d30a6b2046070ecbe558e8462
Alaxsan <Alaxsan.Jeyakumar@vzug.ch>
2024-06-18 10:03:31 +0200

    commit final version

---
+++ .angular/cache/17.3.7/vite/deps/primeng_button.js
@@ -0,0 +1,18 @@
+import {
+  Button,
+  ButtonDirective,
+  ButtonModule
+} from "./chunk-XMQPJNLP.js";
+import "./chunk-XDVZYI3F.js";
+import "./chunk-ZOIFIW6L.js";
+import "./chunk-AVKUJZ5C.js";
+import "./chunk-QYWHIPA4.js";
+import "./chunk-JWGNUHUZ.js";
+import "./chunk-WSA2QMXP.js";
+import "./chunk-J4B6MK7R.js";
+export {
+  Button,
+  ButtonDirective,
+  ButtonModule
+};
+//# sourceMappingURL=primeng_button.js.map
